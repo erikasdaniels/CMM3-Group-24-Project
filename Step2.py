a7124c211f47771249d94966e4a2e324bf7ddcd8
charlie-knott <s2140985@ed.ac.uk>
2024-10-08 16:06:35 +0100

    Update Step2.py

--- Step2.py
+++ Step2.py
@@ -1,7 +1,9 @@
 import yaml
+import matplotlib.pyplot as plt
+import numpy as np
 
 # Load the YAML file
-with open(r"C:\Users\erika\Documents\Engineering\Computational Methods and Modeling Material\Project\Downloads\heat_pump_cop_synthetic_full.yaml", 'r') as file:
+with open(r"C:\Users\chxrl\Documents\1. UNIVERSITY\Year 3\1. Computational Methods\Group Project\heat_pump_cop_synthetic_full.yaml", 'r') as file:
     data = yaml.safe_load(file)
 
 # Extract COP_noisy and outdoor_temp_C into arrays
@@ -9,5 +11,38 @@ cop_values = [entry['COP_noisy'] for entry in data['heat_pump_cop_data']]
 outdoor_temp_values = [entry['outdoor_temp_C'] for entry in data['heat_pump_cop_data']]
 
 # Now cop_values and outdoor_temp_values are Python lists (arrays)
-print("COP values:", cop_values)
-print("Outdoor temperatures:", outdoor_temp_values)
+#print("COP values:", cop_values)
+#print("Outdoor temperatures:", outdoor_temp_values)
+
+plt.plot(outdoor_temp_values,cop_values)
+ 
+plt.cla()
+
+plt.scatter(outdoor_temp_values,cop_values)
+
+
+delta_t = [60 - value for value in outdoor_temp_values]
+inverse_array = [1 / x for x in delta_t]
+
+print(delta_t)
+
+#print(inverse_array)
+
+plt.cla()
+
+np_delta_t = np.array(delta_t)
+np_inverse_array = np.array(inverse_array)
+np_cop_values = np.array(cop_values)
+
+
+
+slope, intercept = np.polyfit(cop_values, inverse_array, 1)
+
+line_of_best_fit = slope * np_cop_values + intercept
+
+plt.scatter(cop_values, inverse_array)
+
+plt.plot(cop_values,line_of_best_fit)
+
+print(slope)
+print(intercept)
