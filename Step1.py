62568ec32e4dbf758335b4446cbafe34fe41ca6d
erikasdaniels <165791742+erikasdaniels@users.noreply.github.com>
2024-10-09 13:31:32 +0100

    Update Step1.py

--- Step1.py
+++ Step1.py
@@ -1,4 +1,3 @@
-
 from datetime import datetime
 from meteostat import Point, Hourly
 
@@ -8,9 +7,19 @@ location = Point(55.9533, -3.1883)
 # Set the time period for data retrieval
 start = datetime(2023, 1, 1, 0) # Start time
 end = datetime(2023, 1, 2, 0) # End time (24-hour period)
+ 
 
 # Fetch hourly temperature data
 data = Hourly(location,start,end)
 data = data.fetch()
+data = data.reset_index()
 
+#Creates a list for ambient temperature values and a list of the hours 
+t_ambient = []
+hours = data["time"].dt.hour
+
+for i in range(0,25):
+    t = data["temp"].iloc[i] + 273 
+    t_ambient.append(t)
+    
 
